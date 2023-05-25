8817b95e17b8efca069adec1bf4f4218cbc9e3a9
Sylvain LE GAL <sylvain.legal@grap.coop>
2023-05-25 09:30:42 +0200

    add text to implement

--- webmail/models/webmail_mail.py
+++ webmail/models/webmail_mail.py
@@ -21,3 +21,22 @@ class WebmailMail(models.Model):
         store=True,
         readonly=True,
     )
+
+    # client.select_folder('INBOX')
+
+    # # search criteria are passed in a straightforward way
+    # # (nesting is supported)
+    # messages = client.search(['NOT', 'DELETED'])
+
+    # # fetch selectors are passed as a simple list of strings.
+    # response = client.fetch(messages, ['FLAGS', 'RFC822.SIZE', 'ENVELOPE'])
+
+    # # `response` is keyed by message id and contains parsed,
+    # # converted response items.
+    # for message_id, data in response.items():
+    #     import pdb; pdb.set_trace()
+    #     print('{id}: {size} bytes, flags={flags}, {subject}'.format(
+    #         id=message_id,
+    #         subject=data[b'ENVELOPE'].subject.decode(),
+    #         size=data[b'RFC822.SIZE'],
+    #         flags=data[b'FLAGS']))
